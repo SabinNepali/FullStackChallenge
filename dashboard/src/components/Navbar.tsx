fd9339ebf7bf28556d901aac36e683c7accb5134
Sabin Nepali <ersabin123@gmail.com>
2023-03-22 15:33:31 +0530

    Removed the Findings tab from the navigation bar. Now the findings component will only be accessible through the click of the details icon in the grid

--- dashboard/src/components/Navbar.tsx
+++ dashboard/src/components/Navbar.tsx
@@ -4,10 +4,6 @@ import { NavLink } from "react-router-dom";
 
 
 const Navbar: React.FC = () => {
-  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    e.preventDefault();
-    alert("Click on details icon inside action column to view findings for the selected security scan.");
-  };
   return (
     <Menu fixed="top">
       <Menu.Item header as={NavLink} exact to="/home" activeClassName="active">Security Scanner</Menu.Item>
@@ -16,9 +12,6 @@ const Navbar: React.FC = () => {
       </Menu.Item>
       <Menu.Item as={NavLink} exact to="/display" activeClassName="active">
         Security Scan List
-      </Menu.Item>
-      <Menu.Item as={NavLink} exact to="/findings" activeClassName="active" onClick={handleClick}>
-        Findings Display
       </Menu.Item>
     </Menu>
   );
