b6ec68956e126f8b68451d173f17727b8a23262f
Sabin Nepali <ersabin123@gmail.com>
2023-03-22 15:40:54 +0530

    Added Graphics for home page

--- dashboard/src/components/Home.tsx
+++ dashboard/src/components/Home.tsx
@@ -6,7 +6,8 @@ function Home() {
   return (
     <div className='HomePage'>
       <Navbar></Navbar><br/><br/>
-      <h1>Welcome to Security Scan Results</h1>
+      <h1>Welcome to Security Scan System</h1>
+      <img src='scan.png' alt='scan logo'></img>
     </div>   
   );
 }
